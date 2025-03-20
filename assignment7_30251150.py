1c61703eb61f9e23161c8ae3c1db6e194834de1a
evaberepikifa <eva.berepiki@ucalgary.ca>
2025-03-20 18:29:26 +0000

    new changes

--- assignment7_30251150.py
+++ assignment7_30251150.py
@@ -16,16 +16,6 @@ assert x_test.shape == (10000, 32, 32, 3)
 assert y_train.shape == (50000, 1)
 assert y_test.shape == (10000, 1)
 
-
-# Display 5 random samples from the training set
-fig, axes = plt.subplots(1, 5, figsize=(12, 3))
-for i in range(5):
-    idx = np.random.randint(0, x_train.shape[0])
-    axes[i].imshow(x_train[idx])
-    axes[i].axis("off")
-plt.tight_layout()
-plt.show()
-
 # Scale images
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
@@ -35,21 +25,26 @@ y_train = to_categorical(y_train, num_classes=100)
 y_test = to_categorical(y_test, num_classes=100)
 
 # Build the CNN model
+
 model = models.Sequential([
-    Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)),
-    MaxPooling2D((2, 2)),
+        Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)),
+        MaxPooling2D((2, 2)),
 
-    Conv2D(64, (3, 3), activation='relu', padding='same'),
-    MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu', padding='same'),
+        MaxPooling2D((2, 2)),
 
-    Conv2D(128, (3, 3), activation='relu', padding='same'),
-    MaxPooling2D((2, 2)),
+        Conv2D(128, (3, 3), activation='relu', padding='same'),
+        MaxPooling2D((2, 2)),
+        
+        #Conv2D(256, (3, 3), activation='relu', padding='same'),
+        #MaxPooling2D((2, 2)),
 
-    Flatten(),
-    Dense(256, activation='relu'),
-    Dropout(0.5),
-    Dense(100, activation='softmax') 
-])
+        Flatten(),
+        Dense(256, activation='relu'),
+        Dropout(0.5),
+        Dense(100, activation='softmax')
+    ])
+
 
 # Compile the model
 model.compile(optimizer='adam',
@@ -57,11 +52,10 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 #add earlystopping
-# Early stopping callback
 early_stopping = EarlyStopping(monitor='val_loss',patience=5,restore_best_weights=True)
 
-# Train the model
-model.fit(x_train, y_train, epochs= 30, batch_size=64, validation_data=(x_test, y_test), callbacks=[early_stopping])
+# Trainning model
+model.fit(x_train, y_train, epochs= 20, batch_size= 64, validation_data=(x_test, y_test), callbacks=[early_stopping])
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f'Test Loss: {loss:.4f}')
